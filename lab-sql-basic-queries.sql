d85ae5d8c9849e17540cb32b8e2da6ffb7d2964b
UOezkan <102797302+UOezkan@users.noreply.github.com>
2024-04-09 22:50:35 +0200

    Add files via upload

---
+++ lab-sql-basic-queries.sql
@@ -0,0 +1,35 @@
+SHOW TABLES;
+
+SELECT * FROM actor;
+
+SELECT * FROM film;
+
+SELECT * FROM customer;
+
+SELECT title FROM film;
+
+SELECT name AS language FROM language;
+
+SELECT first_name FROM staff;
+
+SELECT DISTINCT release_year FROM film;
+
+SELECT COUNT(*) AS store_count FROM store;
+
+SELECT COUNT(*) AS employee_count FROM staff;
+
+SELECT COUNT(*) AS available_films FROM film;
+
+SELECT COUNT(*) AS rented_films FROM rental;
+
+SELECT COUNT(DISTINCT last_name) AS distinct_last_names FROM actor;
+
+SELECT title, length FROM film
+ORDER BY length DESC
+LIMIT 10;
+
+SELECT * FROM actor WHERE first_name = "SCARLETT";
+
+SELECT * FROM film
+WHERE title LIKE '%ARMAGEDDON%' AND length > 100;
+
